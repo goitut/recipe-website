d53144ca0f6bfb0c5ad381e4d963767f52725b25
goitut <goitutriay@gmail.com>
2025-07-08 02:33:10 +0300

    using APIs to fetch data

---
+++ main.js
@@ -0,0 +1,9 @@
+const SEARCH_API_URL = "www.themealdb.com/api/json/v1/1/search.php?s=";
+
+const searchForm = document.getElementById("search-form");
+
+const searchInput = document.getElementById("search-input");
+
+const resultsGrid = document.getElementById("results-grid");
+
+const messageArea = document.getElementById("message-area");
